a5d5901b774e829ced40fc200d6ff5b8830d9e73
Antoine MALGAT <antoinemalgat68@gmail.com>
2025-08-12 14:30:21 +0200

    [+] JSDocs for DBForge

--- lib/DBForge.ts
+++ lib/DBForge.ts
@@ -32,10 +32,18 @@ export default class DBForge {
 
     private statment: string = "";
 
+    /**
+     * Create a new DBForge object.
+     * @returns {DBForge} a new DBForge object.
+     */
     static create(): DBForge {
         return new DBForge();
     }
 
+    /**
+     * Reset the DBForge object. After each request, the DBForge object is also automatically reset.
+     * @returns {this} the same DBForge object.
+     */
     reset(): this {
         this.columns = {
             add: [],
@@ -63,14 +71,30 @@ export default class DBForge {
         return this;
     }
 
+    /**
+     * Create a new database with the specified name if it does not already exist.
+     * @param {string} name - The name of the database to create.
+     * @returns {string} The SQL query statement for creating the database.
+     */
     createDataBase(name: string): string {
         return this.query(`CREATE DATABASE IF NOT EXISTS \`${name}\`;`);
     }
 
+    /**
+     * Drop a database with the specified name if it exists.
+     * @param {string} name - The name of the database to drop.
+     * @returns {string} The SQL query statement for dropping the database.
+     */
     dropDataBase(name: string): string {
         return this.query(`DROP DATABASE IF EXISTS \`${name}\`;`);
     }
 
+    /**
+     * Create a new table with the specified name and optional comment.
+     * @param {string} name - The name of the table to create.
+     * @param {string} [comment=""] - The comment for the table.
+     * @returns {string} The SQL query statement for creating the table.
+     */
     createTable(name: string, comment: string = ""): string {
         let names: { db: string; table: string } = this.explodeName(name);
         let tmp: string = "";
@@ -115,17 +139,34 @@ export default class DBForge {
         return this.query(this.statment);
     }
 
+    /**
+     * Drop a table with the specified name if it exists.
+     * @param {string} name - The name of the table to drop.
+     * @returns {string} The SQL query statement for dropping the table.
+     */
     dropTable(name: string): string {
         name = this.quoteIdentifier(name);
         return this.query(`DROP TABLE IF EXISTS ${name};`);
     }
 
+    /**
+     * Rename a table from the old name to the new name.
+     * @param {string} from - The current name of the table to rename.
+     * @param {string} to - The new name for the table.
+     * @returns {string} The SQL query statement for renaming the table.
+     */
     renameTable(from: string, to: string): string {
         from = this.quoteIdentifier(from);
         to = this.quoteIdentifier(to);
         return this.query(`ALTER TABLE ${from} RENAME TO ${to};`);
     }
 
+    /**
+     * Alter a table with the specified name.
+     * This function can be used to drop, add or modify columns, as well as add or drop indexes and foreign keys.
+     * @param {string} name - The name of the table to alter.
+     * @returns {string} The SQL query statement for altering the table.
+     */
     alterTable(name: string): string {
         let names: { db: string; table: string } = this.explodeName(name);
         let tmp: string = "";
@@ -188,6 +229,13 @@ export default class DBForge {
         return this.query(this.statment);
     }
 
+    /**
+     * Add a new column to the table with the given name.
+     * If a callback is given, it will be called with the new column as an argument.
+     * @param {string} name - The name of the column to add.
+     * @param {null | ((col: DBForgeColumn) => void)} [callback] - A callback to run with the new column.
+     * @returns {this}
+     */
     addColumn(
         name: string,
         callback: null | ((col: DBForgeColumn) => void)
@@ -200,11 +248,26 @@ export default class DBForge {
         return this;
     }
 
+    /**
+     * Drop a column with the given name.
+     * @param {string} name The name of the column to drop.
+     * @returns {this}
+     */
     dropColumn(name: string): this {
         this.columns.drop.push(name);
         return this;
     }
 
+    /**
+     * Change a column with the given new name and configuration. If no new name 
+     * is provided, the column will retain its original name. If a callback is 
+     * provided, it will be called to configure the column
+     * @param {string} name - The current name of the column.
+     * @param {string} [newName] - The new name for the column.
+     * @param {null | ((col: DBForgeColumn) => void)} callback - A callback to 
+     * configure the column.
+     * @returns {this}
+     */
     changeColumn(
         name: string,
         newName: string = "",
@@ -220,6 +283,11 @@ export default class DBForge {
         return this;
     }
 
+    /**
+     * Add a primary key to the table with the given column names.
+     * @param {...string} names - The names of the columns to add as primary key.
+     * @returns {this}
+     */
     addPrimary(...names: string[]): this {
         for (const name of names) {
             this.primary.add.push(`\`${name}\``);
@@ -227,16 +295,34 @@ export default class DBForge {
         return this;
     }
 
+    /**
+     * Drop the primary key of the table.
+     * @returns {this}
+     */
     dropPrimary(): this {
         this.primary.drop = true;
         return this;
     }
 
+    /**
+     * Add an index to the table with the given name and configuration.
+     * @param {string} name - The name of the index to add.
+     * @param {{ name: string; asc?: boolean }[]} [cols] - The columns to add 
+     * as the index.
+     * @returns {this}
+     */
     addIndex(name: string, cols: { name: string; asc?: boolean }[] = []): this {
         this.keys.add.push(this.buildKey(name, false, cols));
         return this;
     }
 
+    /**
+     * Add an unique index to the table with the given name and configuration.
+     * @param {string} name - The name of the unique index to add.
+     * @param {{ name: string; asc?: boolean }[]} [cols] - The columns to add 
+     * as the index.
+     * @returns {this}
+     */
     addUnique(
         name: string,
         cols: { name: string; asc?: boolean }[] = []
@@ -245,16 +331,38 @@ export default class DBForge {
         return this;
     }
 
+    /**
+     * Drop the index with the given name.
+     * @param {string} name - The name of the index to drop.
+     * @returns {this}
+     */
     dropIndex(name: string): this {
         this.keys.drop.push(name + "_idx");
         return this;
     }
 
+    /**
+     * Drop the unique index with the given name.
+     * @param {string} name - The name of the unique index to drop.
+     * @returns {this}
+     */
     dropUnique(name: string): this {
         this.keys.drop.push(name + "_unq");
         return this;
     }
 
+    /**
+     * Add a foreign key constraint to the table.
+     * @param {string} field - The field in the table to create the foreign key
+     * on.
+     * @param {string} target - The table and field to reference. Should be in the
+     * form of "table_name.field_name".
+     * @param {string} [del="CASCADE"] - The action to take when a row is deleted
+     * from the referenced table.
+     * @param {string} [update="CASCADE"] - The action to take when a row is updated
+     * in the referenced table.
+     * @returns {this}
+     */
     addFk(
         field: string,
         target: string,
@@ -278,19 +386,43 @@ export default class DBForge {
         return this;
     }
 
+    /**
+     * Add an index to the foreign key with the field given.
+     * This is a shortcut for `addIndex(field + "_fk", [{ name: field, asc: true }])`
+     * @param {string} field - The field of the foreign key to add the index to.
+     * @returns {this}
+     */
     addFkIndex(field: string): this {
         return this.addIndex(field + "_fk", [{ name: field, asc: true }]);
     }
 
+    /**
+     * Drop the foreign key constraint with the given field.
+     * @param {string} field - The field of the foreign key to drop.
+     * @returns {this}
+     */
     dropFk(field: string): this {
         this.foreign.drop.push(field);
         return this;
     }
 
+    /**
+     * Drop the index on the foreign key with the given field.
+     * This is a shortcut for `dropIndex(field + "_fk")`
+     * @param {string} field - The field of the foreign key to drop the index from.
+     * @returns {this}
+     */
     dropFkIndex(field: string): this {
         return this.dropIndex(field + "_fk");
     }
 
+    /**
+     * Build an index from the given configuration.
+     * @param {string} name - The name of the index.
+     * @param {boolean} unique - Whether the index should be unique.
+     * @param {{ name: string; asc?: boolean }[]} cols - The columns to add as the index.
+     * @returns {{ name: string; unique: boolean; colums: string }} - The index configuration.
+     */
     private buildKey(
         name: string,
         unique: boolean,
@@ -317,15 +449,33 @@ export default class DBForge {
         };
     }
 
+    /**
+     * Quote the given database and table name.
+     * @param {object} names - The database and table name to quote.
+     * @param {string} names.db - The database name.
+     * @param {string} names.table - The table name.
+     * @returns {string} The quoted database and table name.
+     */
     private quoteName(names: { db: string; table: string }): string {
         if (names.db !== "") return `\`${names.db}\`.\`${names.table}\``;
         return `\`${names.table}\``;
     }
 
+    /**
+     * Quote the given identifier (column or table name) by splitting it into its database and table name parts, and then quoting each part.
+     * @param {string} field - The identifier to quote.
+     * @returns {string} The quoted identifier.
+     */
     private quoteIdentifier(field: string): string {
         return this.quoteName(this.explodeName(field));
     }
 
+    /**
+     * Explode the given identifier (column or table name) into its database and table name parts.
+     * If the identifier doesn't contain a dot, it will be considered as a table name.
+     * @param {string} name - The identifier to explode.
+     * @returns {{ db: string; table: string }} - The exploded identifier. The "db" property is the database name, and the "table" property is the table name.
+     */
     private explodeName(name: string): { db: string; table: string } {
         let splited: string[] = name.split(".");
         if (splited.length === 1) {
@@ -334,10 +484,21 @@ export default class DBForge {
         return { db: splited[0], table: splited[1] };
     }
 
+    /**
+     * Quote the given value by replacing single quotes with double quotes and
+     * enclosing it in single quotes.
+     * @param {string} value - The value to quote.
+     * @returns {string} The quoted value.
+     */
     private quote(value: string): string {
         return "'" + value.replaceAll("'", "''") + "'";
     }
 
+    /**
+     * Resets the alter table configuration and returns the given statment.
+     * @param {string} statment - The statment to return.
+     * @returns {string} The given statment.
+     */
     private query(statment: string): string {
         this.reset();
         return statment;
--- lib/DBForge_Column.ts
+++ lib/DBForge_Column.ts
@@ -9,11 +9,22 @@ export default class DBForgeColumn {
     protected firstCol: boolean = false;
     protected commentCol: string = "";
 
+    /**
+     * Constructor of DBForgeColumn
+     * @param {string} name Name of the column
+     */
     constructor(name: string) {
         this.nameCol = name;
         this.varchar();
     }
 
+    /**
+     * Set the type of the column manually. The constraints can be a list of constraints.
+     * If possible avoid using this method and prefer the other methods with predefined constraints.
+     * @param {string} type The type of the column
+     * @param  {...(string|number)} constraints The constraints of the type
+     * @returns {this}
+     */
     type(type: string, ...constraints: (string | number)[]): this {
         this.typeCol = type.toUpperCase();
         if (constraints.length !== 0) {
@@ -22,66 +33,140 @@ export default class DBForgeColumn {
         return this;
     }
 
+    /**
+     * Set the type of the column to boolean. If no default value is given, it will be `false`. It will also make the column unsigned.
+     * @param {boolean} [defaultVal=false] The default value of the column
+     * @returns {this}
+     */
     bool(defaultVal: boolean = false): this {
         return this.type("tinyint", 1)
             .unsigned()
             .defaultValue(defaultVal ? 1 : 0);
     }
 
+    /**
+     * Set the type of the column to int. The prefix can be one of the following: `TINY`, `SMALL`, `MEDIUM`, `BIG`.
+     * If no prefix is given, it will be `INT` by default.
+     * @param {"" | "TINY" | "SMALL" | "MEDIUM" | "BIG"} [prefix=""] The prefix of the type
+     * @returns {this}
+     */
     int(prefix: "" | "TINY" | "SMALL" | "MEDIUM" | "BIG" = ""): this {
         return this.type(prefix + "int");
     }
 
+    /**
+     * Set the type of the column to float.
+     * @returns {this}
+     */
     float(): this {
         return this.type("float");
     }
 
+    /**
+     * Set the type of the column to decimal. If no precision is given, it will be `10`. If no scale is given, it will be `2`.
+     * @param {number} [precision=10] The precision of the decimal
+     * @param {number} [scale=2] The scale of the decimal
+     * @returns {this}
+     */
     decimal(precision: number = 10, scale: number = 2): this {
         return this.type("decimal", precision, scale);
     }
 
+    /**
+     * Set the type of the column to char. The length given will be the length of the char.
+     * @param {number} len The length of the char
+     * @returns {this}
+     */
     char(len: number): this {
         return this.type("char", len);
     }
 
+    /**
+     * Set the type of the column to varchar. If no length is given, it will be `255`.
+     * @param {number} [len=255] The length of the varchar
+     * @returns {this}
+     */
     varchar(len: number = 255): this {
         return this.type("varchar", len);
     }
 
+    /**
+     * Set the type of the column to text. The prefix can be one of the following: `TINY`, `MEDIUM`, `LONG`.
+     * If no prefix is given, it will be `TEXT` by default.
+     * @param {"" | "TINY" | "MEDIUM" | "LONG"} [prefix=""] The prefix of the type
+     * @returns {this}
+     */
     text(prefix: "" | "TINY" | "MEDIUM" | "LONG" = ""): this {
         return this.type(prefix + "text");
     }
 
+    /**
+     * Set the type of the column to blob. The prefix can be one of the following: `TINY`, `MEDIUM`, `LONG`.
+     * If no prefix is given, it will be `BLOB` by default.
+     * @param {"" | "TINY" | "MEDIUM" | "LONG"} [prefix=""] The prefix of the type
+     * @returns {this}
+     */
     blob(prefix: "" | "TINY" | "MEDIUM" | "LONG" = ""): this {
         return this.type(prefix + "blob");
     }
 
+    /**
+     * Set the type of the column to timestamp.
+     * @returns {this}
+     */
     timestamp(): this {
         return this.type("timestamp");
     }
 
+    /**
+     * Set the type of the column to datetime.
+     * @returns {this}
+     */
     datetime(): this {
         return this.type("datetime");
     }
 
+    /**
+     * Set the type of the column to date.
+     * @returns {this}
+     */
     date(): this {
         return this.type("date");
     }
 
+    /**
+     * Set the type of the column to time.
+     * @returns {this}
+     */
     time(): this {
         return this.type("time");
     }
 
+    /**
+     * Make the column unsigned. By default, it will be `true`.
+     * @param {boolean} [value=true] Specifies whether the column should be unsigned.
+     * @returns {this}
+     */
     unsigned(value: boolean = true): this {
         this.unsignedCol = value;
         return this;
     }
 
+    /**
+     * Make the column not nullable. By default, it will be `false`.
+     * @returns {this}
+     */
     notNull(): this {
         this.notNullCol = true;
         return this;
     }
 
+    /**
+     * Set the default value of the column. If quote is set to true, strings will be quoted.
+     * @param {any} value The default value of the column
+     * @param {boolean} [quote=true] Specifies whether the value should be quoted
+     * @returns {this}
+     */
     defaultValue(value: any, quote: boolean = true): this {
         if (quote && typeof value === "string") {
             this.defaultCol =
@@ -93,6 +178,11 @@ export default class DBForgeColumn {
         return this.notNull();
     }
 
+    /**
+     * Set the default value of the column to the current timestamp. If onUpdate is set to true, the column will also be set to the current timestamp on update.
+     * @param {boolean} [onUpdate=false] Specifies whether the column should be set to the current timestamp on update
+     * @returns {this}
+     */
     defaultTimestamp(onUpdate: boolean = false): this {
         return this.defaultValue(
             "CURRENT_TIMESTAMP" +
@@ -101,27 +191,51 @@ export default class DBForgeColumn {
         );
     }
 
+    /**
+     * Make the column auto increment. By default, it will be `true`. If value is set to `false`, the column will not be auto increment.
+     * @param {boolean} [value=true] Specifies whether the column should be auto increment
+     * @returns {this}
+     */
     autoIncrement(value: boolean = true): this {
         this.autoIncrementCol = value;
         return this.notNull();
     }
 
+    /**
+     * Set a comment for the column. The comment will be quoted.
+     * Single quotes within the comment will be escaped.
+     * @param {string} value The comment to set for the column.
+     * @returns {this}
+     */
     comment(value: string): this {
         this.commentCol =
             value !== "" ? "'" + value.replaceAll("'", "''") + "'" : "''";
         return this;
     }
 
+    /**
+     * Add the column at the first position in the table.
+     * @returns {this}
+     */
     first(): this {
         this.firstCol = true;
         return this;
     }
 
+    /**
+     * Add the column after the column with the given name.
+     * @param {string} name The name of the column to add the new column after.
+     * @returns {this}
+     */
     after(name: string): this {
         this.afterCol = name;
         return this;
     }
 
+    /**
+     * Converts the column to a string in the format of a MySQL column definition.
+     * @returns {string} The string representation of the column.
+     */
     toString(): string {
         let str = `\`${this.nameCol}\` ${this.typeCol}`;
 
